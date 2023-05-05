796884462cf73a36a5e10addfa295c81d333ebd0
Darshan Parmar <darshanparmar.nilu@gmail.com>
2023-05-05 18:05:22 +0530

    Segment Control File Add And UI Updates

---
+++ Signup & SignIn/Signup & SignIn/CustomSegment.swift
@@ -0,0 +1,8 @@
+//
+//  CustomSegment.swift
+//  Signup & SignIn
+//
+//  Created by MacBookAir_4 on 05/05/23.
+//
+
+import Foundation
---
+++ Signup & SignIn/Signup & SignIn/TabBarViewController.swift
@@ -0,0 +1,29 @@
+//
+//  TabbarViewController.swift
+//  Signup & SignIn
+//
+//  Created by MacBookAir_4 on 04/05/23.
+//
+
+import UIKit
+
+class TabbarViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
